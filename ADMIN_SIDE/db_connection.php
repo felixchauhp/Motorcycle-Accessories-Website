148cede0dbb5f9347587065974beca53dfa5e1b4
phucnguyenbao <148614801+phucnguyenbao@users.noreply.github.com>
2024-11-18 10:39:22 +0700

    db_connection

--- ADMIN_SIDE/db_connection.php
+++ ADMIN_SIDE/db_connection.php
@@ -1,17 +1,24 @@
-//Kết nối chưa có aiven nhé
 <?php
-// Kết nối MySQL
-$mysqli = new mysqli("localhost", "root", "", "DA_KTDL");
+// Cấu hình kết nối tới cơ sở dữ liệu MySQL trên Aiven
+$host = 'motorcycle-da-ktdl.j.aivencloud.com'; // Thay bằng hostname của Aiven MySQL
+$port = 17160; // Cổng mặc định của MySQL
+$username = 'baophuc'; // Thay bằng tên đăng nhập của bạn
+$password = 'AVNS_Y0CHLEKwLz75-i0dayg'; // Thay bằng mật khẩu của bạn
+$database = 'motorcycle'; // Thay bằng tên cơ sở dữ liệu của bạn
+
+// Kết nối với MySQL
+$conn = new mysqli($host, $username, $password, $database, $port);
 
 // Kiểm tra kết nối
-if ($mysqli->connect_errno) {
-    echo "Không thể kết nối đến MySQL: " . $mysqli->connect_error;
-    exit();
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
 }
+echo "Kết nối thành công!";
+
 
 // Truy vấn dữ liệu từ bảng sản phẩm
 $query = "SELECT * FROM products"; 
-$result = $mysqli->query($query);
+$result = $conn->query($query);
 
 // Kiểm tra kết quả
 if ($result->num_rows > 0) {
@@ -23,5 +30,25 @@ if ($result->num_rows > 0) {
     $products = [];
 }
 
-$mysqli->close();
+$query_category = "SELECT * FROM products_in_category"; 
+$result_category = $conn->query($query_category);
+
+# Kiểm tra kết quả
+if ($result_category->num_rows > 0) {
+    $products_category = [];
+    while ($row = $result_category->fetch_assoc()) {
+        $products_category[] = $row;
+    }
+} else {
+    $products_category = [];
+}
+
+$conn->close();
 ?>
+
+
+
+
+
+
+
