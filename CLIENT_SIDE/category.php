a3792097ff6414aa988676ff101eb5712d9d13ab
Felix Chau <116711540+felixchauhp@users.noreply.github.com>
2024-12-03 13:15:58 +0700

    Phuc update shop.php, add category.php

---
+++ CLIENT_SIDE/category.php
@@ -0,0 +1,70 @@
+<section class="categories container section">
+        <h3 class="section__title"><span>Danh mục</span> Sản phẩm</h3>
+        <div class="categories__container swiper">
+          <div class="swiper-wrapper">
+            <a href="shop.html" class="category__item swiper-slide">
+              <img
+                src="assets/img/cat6.jpg"
+                alt=""
+                class="category__img"
+              />
+              <h3 class="category__title">Ắc quy</h3>
+            </a>
+            <a href="shop.html" class="category__item swiper-slide">
+              <img
+                src="assets/img/cat12.png"
+                alt=""
+                class="category__img"
+              />
+              <h3 class="category__title">Bạc đạn</h3>
+            </a>
+            <a href="shop.html" class="category__item swiper-slide">
+              <img
+                src="assets/img/cat11.jpg"
+                alt=""
+                class="category__img"
+              />
+              <h3 class="category__title">Bố đĩa và bố thắng</h3>
+            </a>
+            <a href="shop.html" class="category__item swiper-slide">
+              <img
+                src="assets/img/cat10.jpg"
+                alt=""
+                class="category__img"
+              />
+              <h3 class="category__title">Nhông sên dĩa</h3>
+            </a>
+            <a href="shop.html" class="category__item swiper-slide">
+              <img
+                src="assets/img/cat1.jpg"
+                alt=""
+                class="category__img"
+              />
+              <h3 class="category__title">Nhớt</h3>
+            </a>
+            <a href="shop.html" class="category__item swiper-slide">
+              <img
+                src="assets/img/cat2.jpg"
+                alt=""
+                class="category__img"
+              />
+              <h3 class="category__title">Vỏ xe và ruột xe</h3>
+            </a>
+            <a href="shop.html" class="category__item swiper-slide">
+              <img
+                src="assets/img/cat5.jpg"
+                alt=""
+                class="category__img"
+              />
+              <h3 class="category__title">Các phụ kiện khác</h3>
+            </a>
+          </div>
+
+          <div class="swiper-button-prev">
+            <i class="fi fi-rs-angle-left"></i>
+          </div>
+          <div class="swiper-button-next">
+            <i class="fi fi-rs-angle-right"></i>
+          </div>
+        </div>
+      </section>
