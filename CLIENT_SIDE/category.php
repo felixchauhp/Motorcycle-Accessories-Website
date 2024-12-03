008791bf6916a4e9cd11fe94b06145af5323cd4d
phucnguyenbao <148614801+phucnguyenbao@users.noreply.github.com>
2024-12-03 13:40:35 +0700

    update

--- CLIENT_SIDE/category.php
+++ CLIENT_SIDE/category.php
@@ -1,8 +1,8 @@
 <section class="categories container section">
-        <h3 class="section__title"><span>Danh mục</span> Sản phẩm</h3>
+        <h3 class="section__title"><span>Danh mục</span> sản phẩm</h3>
         <div class="categories__container swiper">
           <div class="swiper-wrapper">
-            <a href="shop.html" class="category__item swiper-slide">
+            <a href="shop.php?search=&filter=Ắc+quy" class="category__item swiper-slide">
               <img
                 src="assets/img/cat6.jpg"
                 alt=""
@@ -10,7 +10,7 @@
               />
               <h3 class="category__title">Ắc quy</h3>
             </a>
-            <a href="shop.html" class="category__item swiper-slide">
+            <a href="shop.php?search=&filter=Bạc+đạn" class="category__item swiper-slide">
               <img
                 src="assets/img/cat12.png"
                 alt=""
@@ -18,7 +18,7 @@
               />
               <h3 class="category__title">Bạc đạn</h3>
             </a>
-            <a href="shop.html" class="category__item swiper-slide">
+            <a href="shop.php?search=&filter=Bố+đĩa+và+bố+thắng" class="category__item swiper-slide">
               <img
                 src="assets/img/cat11.jpg"
                 alt=""
@@ -26,7 +26,7 @@
               />
               <h3 class="category__title">Bố đĩa và bố thắng</h3>
             </a>
-            <a href="shop.html" class="category__item swiper-slide">
+            <a href="shop.php?search=&filter=Nhông+sên+dĩa" class="category__item swiper-slide">
               <img
                 src="assets/img/cat10.jpg"
                 alt=""
@@ -34,7 +34,7 @@
               />
               <h3 class="category__title">Nhông sên dĩa</h3>
             </a>
-            <a href="shop.html" class="category__item swiper-slide">
+            <a href="shop.php?search=&filter=Nhớt" class="category__item swiper-slide">
               <img
                 src="assets/img/cat1.jpg"
                 alt=""
@@ -42,7 +42,7 @@
               />
               <h3 class="category__title">Nhớt</h3>
             </a>
-            <a href="shop.html" class="category__item swiper-slide">
+            <a href="shop.php?search=&filter=Vỏ+xe+và+ruột+xe" class="category__item swiper-slide">
               <img
                 src="assets/img/cat2.jpg"
                 alt=""
@@ -50,7 +50,7 @@
               />
               <h3 class="category__title">Vỏ xe và ruột xe</h3>
             </a>
-            <a href="shop.html" class="category__item swiper-slide">
+            <a href="shop.php?search=&filter=Các+phụ+kiện+khác" class="category__item swiper-slide">
               <img
                 src="assets/img/cat5.jpg"
                 alt=""
