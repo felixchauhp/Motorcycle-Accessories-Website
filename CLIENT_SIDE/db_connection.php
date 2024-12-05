c0a6d2cfa1c25da43fdc54a6fb8b59772af2b144
phucnguyenbao <148614801+phucnguyenbao@users.noreply.github.com>
2024-12-05 21:57:49 +0700

    Update db_connection.php

--- CLIENT_SIDE/db_connection.php
+++ CLIENT_SIDE/db_connection.php
@@ -1,9 +1,11 @@
 <?php
 // Cấu hình kết nối tới cơ sở dữ liệu MySQL trên Aiven
-$host = 'motorcycle-da-ktdl.j.aivencloud.com:17160';
-$dbname = 'motorcycle';
-$user = 'caotuan';
-$password = 'AVNS_hti9_ONmu8qTVi8uTAl';
+$host = 'motorcycle-da-ktdl.j.aivencloud.com'; // Thay bằng hostname của Aiven MySQL
+$port = 17160; // Cổng mặc định của MySQL
+$username = 'baophuc'; // Thay bằng tên đăng nhập của bạn
+$password = 'AVNS_Y0CHLEKwLz75-i0dayg'; // Thay bằng mật khẩu của bạn
+$database = 'motorcycle'; // Thay bằng tên cơ sở dữ liệu của bạn
+
 
 
 // Kết nối với MySQL
